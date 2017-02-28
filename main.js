c99ead604815eb041ea380d590162032f1dc0b85
Super-Baleine <paul.feuvraux@muonium.ch>
2017-02-28 16:49:48 +0100

    put in functions prettier
    Signed-off-by: Super-Baleine <paul.feuvraux@muonium.ch>

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 'use strict';
-
 var message = [
 	[3],
 	[2], //j : column
 	[1]
 ];
 var block = [
-	[20, 16, 7], //i : row in a j column
+	[20, 16, 7], //i : row in a j column, block[0][2] = 16
 	[10, 2, 3],
 	[25, 17, 21]
 ];
-// console.log(block[0][1]); --> 16
-var x = [
-	[],
-	[],
-	[]
-];
-
-var i = 0;
-var j = 0;
-
-while (j < message.length) {
-	while (i < block.length) {
-		x[j][i] = message[j]*block[j][i];
-		i++;
-		console.log(x);
+var enc = function(message, block){
+	var i = 0;
+	var j = 0;
+	var x = [
+		[],
+		[],
+		[]
+	]
+	while (j < message.length) {
+		while (i < block.length) {
+			x[j][i] = message[j]*block[j][i];
+			i++;
+			console.log(x);
+		}
+		j++;
+		i = 0;
 	}
-	j++;
-	i = 0;
 }
+
+enc(message, block);
